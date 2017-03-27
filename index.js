a6517972a007a513fe9e8de9e580a791752feebd
Rebekah Apelt <rebekah_apelt@thoughtworks.com>
2017-03-27 20:40:37 +1000

    Blarg

--- index.js
+++ index.js
@@ -13,15 +13,19 @@ mongoose.connect(`mongodb://${username}:${password}@ds135680.mlab.com:35680/hapk
 
 var app = express();
 
-app.use('/*', function(req, res, next) {
+/*app.post('/!*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.header('Access-Control-Allow-Credentials', false);
   next();
-});
+});*/
 
-app.use(cors());
+const corsOptions = {
+  origin: 'http://localhost:8100'
+}
+
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
