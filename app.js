bb89d8fc28a2a4d9af45ba8fbdcb8b1d8b5213b9
Rebekah Apelt <rebekah_apelt@thoughtworks.com>
2017-07-13 14:36:10 +1000

    Update Package.json

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const awsServerlessExpress = require('aws-serverless-express');
 const app = require('./index');
 const server = awsServerlessExpress.createServer(app);
+console.log("App.js");
 
 exports.handler = (event, context) => {
     awsServerlessExpress.proxy(server, event, context);
