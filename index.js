376f0a26ad5b64d004d99dd6194eb84b35828951
Rebekah Apelt <rebekah_apelt@thoughtworks.com>
2017-07-14 09:44:23 +1000

    Added more logging

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ require('dns').resolve('www.google.com', function(err) { if (err) { console.log(
 
 //Mongo DB
 var options = {
-    server: { socketOptions: { keepAlive: 0, connectTimeoutMS: 0 } },
-    replset: { socketOptions: { keepAlive: 0, connectTimeoutMS : 0 } }
+    server: { socketOptions: { keepAlive: 30000, connectTimeoutMS: 30000 } },
+    replset: { socketOptions: { keepAlive: 30000, connectTimeoutMS : 30000 } }
 };
 
 var mongodbUri = 'mongodb://' + process.env.USER + ':' + process.env.PW + '@ds135680.mlab.com:35680/hapkido';
@@ -24,33 +24,32 @@ mongoose.connect(mongodbUri, options);
 var conn = mongoose.connection;
 
 conn.on('error', function(err) {
-    console.log(err);
+    console.log("This is my error for mongo db", err);
 });
 
 conn.once('open', function() {
-    console.log("Mongo Connected");
     // Wait for the database connection to establish, then start the app.
-
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({
-        extended: true
-    }));
-
-    app.use(awsServerlessExpressMiddleware.eventContext());
-
-    app.use("/student", router);
-
-    if(process.env.LH === true) {
-        var server = http.createServer(app);
-        var port = process.env.PORT || 4000;
-        var host = process.env.HOST || '127.0.0.1';
-        console.log(port, host);
-
-        server.listen(port, host);
-    }
+    console.log("Mongo Connected");
 
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(awsServerlessExpressMiddleware.eventContext());
+
+app.use("/student", router);
+
+if(process.env.LH === true) {
+    var server = http.createServer(app);
+    var port = process.env.PORT || 4000;
+    var host = process.env.HOST || '127.0.0.1';
+    console.log(port, host);
+
+    server.listen(port, host);
+}
 //End Mongo Db
 
 
