9037a4d5b420c4fdbe4283e7c04b05f858a97eb4
Rebekah Apelt <rebekah_apelt@thoughtworks.com>
2017-07-14 10:02:43 +1000

    Moved Mongo back inside connect

--- index.js
+++ index.js
@@ -30,27 +30,28 @@ conn.on('error', function(err) {
 conn.once('open', function() {
     // Wait for the database connection to establish, then start the app.
     console.log("Mongo Connected");
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+
+    app.use(awsServerlessExpressMiddleware.eventContext());
+
+    app.use("/student", router);
+
+    if(process.env.LH === true) {
+        var server = http.createServer(app);
+        var port = process.env.PORT || 4000;
+        var host = process.env.HOST || '127.0.0.1';
+        console.log(port, host);
+
+        server.listen(port, host);
+    }
+//End Mongo Db
 
 });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 
-app.use(awsServerlessExpressMiddleware.eventContext());
-
-app.use("/student", router);
-
-if(process.env.LH === true) {
-    var server = http.createServer(app);
-    var port = process.env.PORT || 4000;
-    var host = process.env.HOST || '127.0.0.1';
-    console.log(port, host);
-
-    server.listen(port, host);
-}
-//End Mongo Db
 
 
 
