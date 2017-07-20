9213534e1b8248d5e132e03e596316e3079e04d0
Rebekah Apelt <rebekah_apelt@thoughtworks.com>
2017-07-20 10:58:07 +1000

    checking if connection is already open

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 exports.dbconnect = function (callback) {
+    console.log(mongoose.connection.readyState);
     var mongodbUri = 'mongodb://' + process.env.USER + ':' + process.env.PW + '@ds135680.mlab.com:35680/hapkido';
 
     mongoose.connect(mongodbUri, function(err, db) {
