c6400a530c9d99cc1bfd94214d51b7b4bd33ab57
Rebekah Apelt <rebekah_apelt@thoughtworks.com>
2017-07-20 11:02:59 +1000

    Blarg

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,17 +1,20 @@
 var mongoose = require('mongoose');
 
 exports.dbconnect = function (callback) {
-    console.log(mongoose.connection.readyState);
-    var mongodbUri = 'mongodb://' + process.env.USER + ':' + process.env.PW + '@ds135680.mlab.com:35680/hapkido';
+    if(mongoose.connection.readyState === 0){
+        var mongodbUri = 'mongodb://' + process.env.USER + ':' + process.env.PW + '@ds135680.mlab.com:35680/hapkido';
 
-    mongoose.connect(mongodbUri, function(err, db) {
-        if (err) {
-            console.log('Unable to connect to the server. Please start the server. Error:', err);
-        } else {
-            console.log('Connected to Server successfully!');
-            callback();
-        }
-    });
+        mongoose.connect(mongodbUri, function(err, db) {
+            if (err) {
+                console.log('Unable to connect to the server. Please start the server. Error:', err);
+            } else {
+                console.log('Connected to Server successfully!');
+                callback();
+            }
+        });
+    }
+
+
 };
 
 exports.dbdisconnect = function () {
