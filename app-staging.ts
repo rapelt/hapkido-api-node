a8c18ab4a4bdd867bbb806cacf5c7dfcef7dd37e
Rebekah Higgins <rebekahapelt@gmail.com>
2020-03-10 13:34:25 +1000

    Staging updates to remove mongo

--- app-staging.ts
+++ app-staging.ts
@@ -4,15 +4,10 @@ export {};
 const awsServerlessExpress = require('aws-serverless-express');
 const app = require('./index');
 const server = awsServerlessExpress.createServer(app);
-const db = require('./db/dbConnection');
-const config = require('./config/config.staging');
 const mysql = require('./db/rdsconnect');
 
 exports.handler = (event: any, context: any) => {
-    db.dbconnect(function () {
-        console.log("Connected to Db");
-    }, config.dbLocation);
-
     mysql.mysqlconnect().then(() => {
         awsServerlessExpress.proxy(server, event, context);
-    });};
+    });
+};
--- app.js
+++ app.js
@@ -2,9 +2,6 @@
 const awsServerlessExpress = require('aws-serverless-express');
 const app = require('./index');
 const server = awsServerlessExpress.createServer(app);
-var db = require('./db/dbConnection');
-
-const config = require('./config/config.dev');
 var mysql = require('./db/rdsconnect');
 
 exports.handler = (event, context) => {
