66ae9736e773280922a98ae1dd24b090f3fe69a0
Rebekah Higgins <rebekahapelt@gmail.com>
2020-03-10 11:34:19 +1000

    Update app-staging to ts

--- app-staging.ts
+++ app-staging.ts
@@ -2,9 +2,9 @@
 const awsServerlessExpress = require('aws-serverless-express');
 const app = require('./index');
 const server = awsServerlessExpress.createServer(app);
-var db = require('./db/dbConnection');
+const db = require('./db/dbConnection');
 const config = require('./config/config.staging');
-var mysql = require('./db/rdsconnect');
+const mysql = require('./db/rdsconnect');
 
 exports.handler = (event, context) => {
     db.dbconnect(function () {
