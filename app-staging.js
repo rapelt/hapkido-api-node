1ff637dd23bbdac75586685705849c320f605747
Rebekah Higgins <rebekahapelt@gmail.com>
2020-11-01 09:05:01 +1000

    Restarted app.js

--- app-staging.js
+++ app-staging.js
@@ -1,56 +1,68 @@
 'use strict';
-import {createConnection, getConnectionManager} from "typeorm";
 const awsServerlessExpress = require('aws-serverless-express');
-import * as ormconfig from "./ormconfig";
+import {connectDB} from "./db/typeorm-connect";
+const apps = require('./index');
+const server = awsServerlessExpress.createServer(apps);
 
 
-let counter = 0;
 
 exports.handler = (event, context, callback) => {
-    context.callbackWaitsForEmptyEventLoop = false;
+    const startServer = async () => {
+        awsServerlessExpress.proxy(server, event, context);
+    };
 
-    const apps = require('./index');
-    const server = awsServerlessExpress.createServer(apps);
-    awsServerlessExpress.proxy(server, event, context);
-
-    counter++
-    console.log(counter)
-    callback(null, { count: counter })
-
-    const connectionManager = getConnectionManager();
-
-    if(connectionManager.connections.length === 0){
-            createConnection().then(connection => {
-                console.log('TypeORM is connected: ', connection.isConnected);
-            }).catch((err) => {
-                console.log('Connection Error', err);
-            })
-        } else {
-        connectionManager.getConnection(ormconfig).then(connection => {
-            console.log('TypeORM is connected: ', connection.isConnected);
-        });
-        console.log("connections", connectionManager.connections.length);
-    }
-
-
-    // createConnection().then(connection => {
-    //
-    //     awsServerlessExpress.proxy(server, event, context);
-    //
-    //     //var app = apps.listen(port, () => {
-    //
-    //         // const io = require('./src/io/io').init(app);
-    //         //
-    //         // io.on('connection', (socket: any) => {
-    //         //     console.log('Client Connected');
-    //         // });
-    //         //
-    //         // io.emit('posts', {message: 'I am connected'});
-    //
-    //         // io.connect(app, null).then(() => {
-    //         //     console.log(`IO is connected`);
-    //         // });
-    //     //});
+    (async () => {
+        await connectDB();
+        await startServer();
+    })();
 
 
 };
+
+// const server = awsServerlessExpress.createServer(apps);
+// awsServerlessExpress.proxy(server, event, context);
+
+
+// createConnection().then(connection => {
+//
+//     awsServerlessExpress.proxy(server, event, context);
+//
+//     //var app = apps.listen(port, () => {
+//
+//         // const io = require('./src/io/io').init(app);
+//         //
+//         // io.on('connection', (socket: any) => {
+//         //     console.log('Client Connected');
+//         // });
+//         //
+//         // io.emit('posts', {message: 'I am connected'});
+//
+//         // io.connect(app, null).then(() => {
+//         //     console.log(`IO is connected`);
+//         // });
+//     //});
+
+
+//let counter = 0;
+
+// counter++;
+// console.log(counter)
+// callback(null, { count: counter })
+// context.callbackWaitsForEmptyEventLoop = false;
+
+
+
+// const connectionManager = getConnectionManager();
+//
+// if(connectionManager.connections.length === 0){
+//     createConnection().then(connection => {
+//         console.log('TypeORM is connected: ', connection.isConnected);
+//     }).catch((err) => {
+//         console.log('Connection Error', err);
+//     })
+// } else {
+//     connectionManager.getConnection(ormconfig).then(connection => {
+//         console.log('TypeORM is connected: ', connection.isConnected);
+//     });
+//     console.log("connections", connectionManager.connections.length);
+// }
