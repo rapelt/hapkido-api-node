7156b51c440f03db40c02c311f8a0ab50cd795ee
Rebekah Higgins <rebekahapelt@gmail.com>
2020-10-31 20:05:29 +1000

    Removed table specifics from Other migraton

--- migration/1604134844001-Other.ts
+++ migration/1604134844001-Other.ts
@@ -4,11 +4,11 @@ export class Other1604134844001 implements MigrationInterface {
     name = 'Other1604134844001'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("CREATE TABLE `hapkido_local_2`.`query-result-cache` (`id` int NOT NULL AUTO_INCREMENT, `identifier` varchar(255) NULL, `time` bigint NOT NULL, `duration` int NOT NULL, `query` text NOT NULL, `result` text NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB");
+        await queryRunner.query("CREATE TABLE `query-result-cache` (`id` int NOT NULL AUTO_INCREMENT, `identifier` varchar(255) NULL, `time` bigint NOT NULL, `duration` int NOT NULL, `query` text NOT NULL, `result` text NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB");
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("DROP TABLE `hapkido_local_2`.`query-result-cache`");
+        await queryRunner.query("DROP TABLE `query-result-cache`");
     }
 
 }
