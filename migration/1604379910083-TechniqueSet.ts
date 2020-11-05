5258dd36618009539c1e5863ea617ddfded4baad
Rebekah Higgins <rebekahapelt@gmail.com>
2020-11-06 09:55:44 +1000

    removed drop index

--- migration/1604379910083-TechniqueSet.ts
+++ migration/1604379910083-TechniqueSet.ts
@@ -4,8 +4,8 @@ export class TechniqueSet1604379910083 implements MigrationInterface {
     name = 'TechniqueSet1604379910083'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("DROP INDEX `IDX_46fbf76bbdb372850e89894881` ON `class`");
-        await queryRunner.query("DROP INDEX `IDX_0add4cdd0d981761402303e054` ON `technique_set`");
+        // await queryRunner.query("DROP INDEX `IDX_46fbf76bbdb372850e89894881` ON `class`");
+        // await queryRunner.query("DROP INDEX `IDX_0add4cdd0d981761402303e054` ON `technique_set`");
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
