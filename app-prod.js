1a239a900f23978f47624439493bbfb77b8d0a93
Rebekah Higgins <rebekahapelt@gmail.com>
2020-11-18 18:15:02 +1000

    Io in staging

--- app-prod.js
+++ app-prod.js
@@ -8,6 +8,19 @@ const server = awsServerlessExpress.createServer(apps);
 exports.handler = (event, context, callback) => {
     const startServer = async () => {
         awsServerlessExpress.proxy(server, event, context);
+
+        const io = require('./src/io/io').init(server);
+
+        io.on('connection', (socket) => {
+            console.log('Client Connected');
+
+            io.emit('posts', {message: 'I am connected'});
+
+        });
+
+        io.on('connect_failed', function() {
+            console.log("Sorry, there seems to be an issue with the connection!");
+        })
     };
 
     (async () => {
