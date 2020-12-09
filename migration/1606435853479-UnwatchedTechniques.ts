5e68f82dcdec2f36786d883645fd860a6aa2cea4
Rebekah Higgins <rebekahapelt@gmail.com>
2020-12-09 10:02:04 +1000

    Update migrations

--- migration/1606435853479-UnwatchedTechniques.ts
+++ migration/1606435853479-UnwatchedTechniques.ts
@@ -5,13 +5,9 @@ export class UnwatchedTechniques1606435853479 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query("CREATE TABLE `unwatched_techniques` (`id` int NOT NULL AUTO_INCREMENT, `createdAt` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updatedAt` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `deletedAt` datetime(6) NULL, `t_id` int NULL, `hb_id` varchar(6) NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB");
-        await queryRunner.query("ALTER TABLE `unwatched_techniques` ADD CONSTRAINT `FK_d83497f7e374e3d0293d629319f` FOREIGN KEY (`t_id`) REFERENCES `technique`(`t_id`) ON DELETE NO ACTION ON UPDATE NO ACTION");
-        await queryRunner.query("ALTER TABLE `unwatched_techniques` ADD CONSTRAINT `FK_8101d2fe00519df31d4889282ef` FOREIGN KEY (`hb_id`) REFERENCES `member`(`hb_id`) ON DELETE NO ACTION ON UPDATE NO ACTION");
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `unwatched_techniques` DROP FOREIGN KEY `FK_8101d2fe00519df31d4889282ef`");
-        await queryRunner.query("ALTER TABLE `unwatched_techniques` DROP FOREIGN KEY `FK_d83497f7e374e3d0293d629319f`");
         await queryRunner.query("DROP TABLE `unwatched_techniques`");
     }
 
--- migration/1607057840545-Favourite.ts
+++ migration/1607057840545-Favourite.ts
@@ -5,13 +5,9 @@ export class Favourite1607057840545 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query("CREATE TABLE `favourite_techniques` (`id` int NOT NULL AUTO_INCREMENT, `createdAt` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updatedAt` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `deletedAt` datetime(6) NULL, `t_id` int NULL, `hb_id` varchar(6) NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB");
-        await queryRunner.query("ALTER TABLE `favourite_techniques` ADD CONSTRAINT `FK_cce4775fcf023ed3d1993567855` FOREIGN KEY (`t_id`) REFERENCES `technique`(`t_id`) ON DELETE NO ACTION ON UPDATE NO ACTION");
-        await queryRunner.query("ALTER TABLE `favourite_techniques` ADD CONSTRAINT `FK_7b1cfccbaf69e406d1cfec047b4` FOREIGN KEY (`hb_id`) REFERENCES `member`(`hb_id`) ON DELETE NO ACTION ON UPDATE NO ACTION");
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query("ALTER TABLE `favourite_techniques` DROP FOREIGN KEY `FK_7b1cfccbaf69e406d1cfec047b4`");
-        await queryRunner.query("ALTER TABLE `favourite_techniques` DROP FOREIGN KEY `FK_cce4775fcf023ed3d1993567855`");
         await queryRunner.query("DROP TABLE `favourite_techniques`");
     }
 
